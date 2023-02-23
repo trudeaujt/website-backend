a4087ac7d3c05665afd6cff235cb5d896ba30e0e
Jonathan Trudeau <48639690+trudeaujt@users.noreply.github.com>
2023-02-23 11:49:33 +0900

    Add port flag

--- cmd/webserver/main.go
+++ cmd/webserver/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"github.com/trudeaujt/blogposts"
 	"log"
 	"net/http"
@@ -10,13 +11,16 @@ import (
 
 func main() {
 	dirFlag := flag.String("directory", "/var/posts", "directory containing blogposts")
+	portFlag := flag.String("port", "5001", "port to listen on")
 	flag.Parse()
 
 	dir := os.DirFS(*dirFlag)
 	posts, _ := blogposts.NewPostsFromFS(dir)
 	server := blogposts.NewBlogServer(posts)
 
-	if err := http.ListenAndServe(":5001", server); err != nil {
+	port := fmt.Sprintf(":%v", *portFlag)
+
+	if err := http.ListenAndServe(port, server); err != nil {
 		log.Fatalf("could not listen on port 5001 %v", err)
 	}
 }
